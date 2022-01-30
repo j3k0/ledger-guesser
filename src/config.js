303dcb228f8cc4e77bc4e1ff7353ad2ccb609190
Jean-Christophe Hoelt <hoelt@fovea.cc>
2022-01-30 16:41:00 +0000

    Decrease max error rate

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const log = require('./log');
 
 module.exports = {
   trainingSettings: {
-    iterations: 500,        // the maximum times to iterate the training data
-    errorThresh: 0.0004,    // the acceptable error percentage from training data
+    iterations: 1000,        // the maximum times to iterate the training data
+    errorThresh: 0.0002,    // the acceptable error percentage from training data
     log: log.debug.bind(log) // true to use console.log, when a function is supplied it is used
   },
   hiddenLayers: [],
